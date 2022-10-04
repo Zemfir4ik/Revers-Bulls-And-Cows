e0935f0da246dc4587a221a1406bfb1969041cc4
Zemfir4ik <klientkey2608@gmail..com>
2022-10-05 00:29:31 +0300

    some troubles

--- main.cpp
+++ main.cpp
@@ -6,49 +6,44 @@
 using namespace std;
 
 void show(int arr[4]);
+void randomGuess(int *computerNum);
+void makeNewGuess(int bulls, int cows, int *computerNum, int *tempGuess);
+int **allVariation();
 
 int main()
 {
-    int computerNum[4];
-    int bulls;
-    int cows;
+    // int computerNum[4];
+    // int tempGuess[4];
+    // int bulls;
+    // int cows = 0;
 
-    srand(time(NULL));
+    // randomGuess(computerNum);
+    // show(computerNum);
 
-    while (true)
+    // while (true)
+    // {
+    //     makeNewGuess(bulls, cows, computerNum, tempGuess);
+
+    //     cout << "Сколько быков?" << endl;
+    //     cin >> bulls;
+
+    //     if (bulls == 4)
+    //     {
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         cout << "Сколько коров?" << endl;
+    //         cin >> cows;
+    //     }
+    // }
+
+    int **arr = allVariation();
+
+
+    for (int i = 0; i < 5040; ++i)
     {
-
-        // создание рандомного набора чисел
-        for (int i = 0; i < 4; ++i)
-        {
-            computerNum[i] = rand() % 10;
-
-            for (int c = 0; c < 5; ++c)
-            {
-                for (int j = 0; j <= i; ++j)
-                {
-                    if (computerNum[i] == computerNum[i - j])
-                    {
-                        computerNum[i] += 1;
-                        computerNum[i] %= 10;
-                    }
-                }
-            }
-        }
-        
-        show(computerNum);
-
-        cout << "Сколько быков?" << endl;
-        cin >> bulls;
-        {
-            if (bulls == 4)
-            {
-                break;
-            }
-        }
-
-        cout << "Сколько коров?" << endl;
-        cin >> cows;
+        show(*arr);
     }
 }
 
@@ -59,4 +54,78 @@ void show(int arr[4])
         cout << arr[i];
     }
     cout << endl;
+}
+
+void randomGuess(int *computerNum)
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        computerNum[i] = rand() % 10;
+
+        for (int c = 0; c < 5; ++c)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (computerNum[i] == computerNum[j])
+                {
+                    computerNum[i] += 1;
+                    computerNum[i] %= 10;
+                }
+            }
+        }
+    }
+}
+
+void makeNewGuess(int bulls, int cows, int *computerNum, int *tempGuess)
+{
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     f
+    // }
+}
+
+int **allVariation()
+{
+    int **arr;
+
+        getchar();
+        for (int a = 0; a < 10; ++a)
+        {
+            for (int b = 0; b < 10; ++b)
+            {
+                if (b == a)
+                {
+                    continue;
+                }
+                else
+                {
+                    for (int c = 0; c < 10; ++c)
+                    {
+                        if (c == a || c == b)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            for (int d = 0; d < 10; ++d)
+                            {
+                                if (d == a || d == b || d == c)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    // arr[i][0] = a;
+                                    // arr[i][1] = b;
+                                    // arr[i][2] = c;
+                                    // arr[i][3] = d;
+                                    cout << a * 1000 + b * 100 + c * 10 + d << endl;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    return arr;
 }
